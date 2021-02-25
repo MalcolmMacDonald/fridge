833c401e5ed83e401698133f5e96daff1685f8bb
MalcolmMacDonald5 <malcolm@hyperhippo.ca>
2021-02-24 21:23:20 -0800

    Added retrieval of discord images.

---
+++ discordInterface.js
@@ -0,0 +1,21 @@
+﻿import './discord.11.6.4.min.js';
+const client = new Discord.Client();
+
+GetFridgeImageURLs(new URLSearchParams(window.location.search).get('key'));
+
+async function GetFridgeImageURLs(BOT_TOKEN) {
+    return new Promise(resolve => {
+            client.login(BOT_TOKEN).then(() => {
+
+                var fridgeChannel = client.channels.get('814021494069395466');
+
+                fridgeChannel.fetchMessages().then(messages => {
+                    var imageAttachments = messages.filter(message => message.attachments.array().length > 0).map(message => message.attachments.map(attachment => attachment.url)[0]);
+                    console.log(imageAttachments);
+                    resolve(imageAttachments);
+                });
+            });
+        }
+    );
+
+}
--- scene.js
+++ scene.js
@@ -1,53 +1,42 @@
-﻿
-import {OrbitControls} from "./OrbitControls.js";
+﻿import {OrbitControls} from "./OrbitControls.js";
 
-import { GLTFLoader } from './GLTFLoader.js';
+import {GLTFLoader} from './GLTFLoader.js';
+
+import {TextureLoader} from "./three.module.js";
 
 const scene = new THREE.Scene();
 
-let  hlight,directionalLight,light,light2,light3,light4;
+let hlight, directionalLight, light, light2, light3, light4;
 
-const  renderer = new THREE.WebGLRenderer({antialias:true});
+const renderer = new THREE.WebGLRenderer({antialias: true});
 document.body.appendChild(renderer.domElement);
 
-const camera =  new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,5000);
+const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.001, 5000);
 
-const controls = new OrbitControls(camera,renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.addEventListener('change', render);
 
-
-async function getFridgeImages(){
-    var token = window.location.href.split("?l=")[1];
-    var header = "Bot " + token;
-    var authorization = {'Authorization':header};
-    var urlString = 'https://discord.com/api' + '/channels/' + '689278471801012286' + '/messages';
-    let response = await fetch (urlString,Headers=authorization);
-    let data = await response.json();
-    return data;
-}
-
-getFridgeImages();
 
 function init() {
 
     scene.background = new THREE.Color(0x36393f);
 
 
-    camera.position.set(0,0,3);
-    camera.lookAt(new THREE.Vector3(0,0,0));
+    camera.position.set(-1, -0.5, 2);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-    hlight = new THREE.AmbientLight (0x021670,0.75);
-    scene.add(hlight);
+    // hlight = new THREE.AmbientLight (0x021670,0.75);
+    // scene.add(hlight);
 
-    directionalLight = new THREE.DirectionalLight(0xffffff,0.75);
-    directionalLight.position.set(10,10,10);
-    directionalLight.lookAt(new THREE.Vector3(0,0,0));
+    directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    directionalLight.position.set(10, 10, 10);
+    directionalLight.lookAt(new THREE.Vector3(0, 0, 0));
 
     directionalLight.castShadow = true;
     scene.add(directionalLight);
-    light = new THREE.PointLight(0xc4c4c4,0.75);
+    light = new THREE.PointLight(0xc4c4c4, 0.75);
     light.castShadow = true;
-    light.position.set(-1,0,4);
+    light.position.set(-1, 0, 4);
     scene.add(light);
     /*light2 = new THREE.PointLight(0xc4c4c4,5);
     light2.position.set(500,100,0);
@@ -59,21 +48,53 @@ function init() {
     light4.position.set(-500,300,500);
     scene.add(light4);*/
 
-    renderer.setSize(window.innerWidth,window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
     let loader = new GLTFLoader();
     loader.load('./models/kitchenFridgeLarge' +
-        '.glb', function(gltf){
-        gltf.scene.position.set(-0.25,-0.5,0);
+        '.glb', function (gltf) {
+        gltf.scene.position.set(-0.25, -0.5, 0);
         //let car = gltf.scene.children[0];
-       // car.scale.set(2,2,2);
+        // car.scale.set(2,2,2);
         scene.add(gltf.scene);
         render();
     });
+    AddImage('https://cdn.discordapp.com/attachments/814021494069395466/814032080811458590/86757885_10157913009857510_1262544196875583488_o.jpg');
     // animate();
 }
+
+async function AddImage(url) {
+    var geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
+    var material = await GetMaterial(url);
+
+    var plane = new THREE.Mesh(geometry, material);
+    scene.add(plane);
+}
+
+async function GetMaterial(url) {
+
+    return new Promise(success => {
+            var loader = new THREE.TextureLoader();
+            loader.load(url, texture => {
+                var mat = new THREE.MeshBasicMaterial({map:texture,side: THREE.DoubleSide});
+                success(mat);
+
+            });
+        });
+}
+function LoadDiscordImage(url){
+    return new Promise(success =>{
+        fetch(url,{Headers:{'Access-Control-Allow-Origin':'*','Access-Control-Allow-Headers':'*'},mode:'cors'}).then(result =>{
+           success(result); 
+        });
+        
+    });
+}
+
 function render() {
-    renderer.render(scene,camera);
+    renderer.render(scene, camera);
     //requestAnimationFrame(render);
 }
+
+
 init();
