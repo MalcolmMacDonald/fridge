8100bd52f1924bee51f2fc268af31d3ecd94b8bc
MalcolmMacDonald5 <malcolm@hyperhippo.ca>
2021-02-25 17:04:27 -0800

    Added positions for images on fridge

--- discordInterface.js
+++ discordInterface.js
@@ -1,16 +1,16 @@
 ﻿import './discord.11.6.4.min.js';
 const client = new Discord.Client();
 
-GetFridgeImageURLs(new URLSearchParams(window.location.search).get('key'));
 
-async function GetFridgeImageURLs(BOT_TOKEN) {
+export async function GetFridgeImageFileNames(BOT_TOKEN) {
     return new Promise(resolve => {
             client.login(BOT_TOKEN).then(() => {
 
-                var fridgeChannel = client.channels.get('814021494069395466');
+                var fridgeChannel = client.channels.get('790124464254877706');
 
                 fridgeChannel.fetchMessages().then(messages => {
                     var imageAttachments = messages.filter(message => message.attachments.array().length > 0).map(message => message.attachments.map(attachment => attachment.url)[0]);
+                    imageAttachments = imageAttachments.map(url => url.split('/').slice(4).join('/'));
                     console.log(imageAttachments);
                     resolve(imageAttachments);
                 });
--- scene.js
+++ scene.js
@@ -4,6 +4,8 @@ import {GLTFLoader} from './GLTFLoader.js';
 
 import {TextureLoader} from "./three.module.js";
 
+import {GetFridgeImageFileNames} from "./discordInterface.js";
+
 const scene = new THREE.Scene();
 
 let hlight, directionalLight, light, light2, light3, light4;
@@ -11,10 +13,41 @@ let hlight, directionalLight, light, light2, light3, light4;
 const renderer = new THREE.WebGLRenderer({antialias: true});
 document.body.appendChild(renderer.domElement);
 
-const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.001, 5000);
+var camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.001, 5000);
 
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', UpdateSize);
 controls.addEventListener('change', render);
+
+const imagePositions = [
+    [-0.165, -0.145,0.75],
+    [-0.1, -0.195,0.75],
+    [-0.16, -0.21,0.75],
+    [-0.145, -0.27,0.75],
+    [-0.145, -0.33,0.75],
+    [-0.08, -0.26,0.75],
+    [-0.06, -0.32,0.75],
+    [0.05, -0.29,0.75],
+    [0.15, -0.36],
+    [0.175, -0.26],
+    [0.02, -0.22],
+    [0.14, -0.175],
+    [0.1, -0.09],
+    [0.085, -0.005],
+    [0.175, 0.005,1],
+    [0.085, 0.075],
+    [-0.123, 0.15,0.8],
+    [0.175, 0.1,1.35],
+    [0.075, 0.155,0.8],
+    [0.16, 0.2],
+    [0.175, 0.26,0.2],
+    [0.15, 0.28,0.2],
+    [0.04, 0.29,0.5],
+    [0.07, 0.23],
+    [0.1, 0.35],
+    [-0.09, 0.23],
+    [-0.14, 0.35]
+]
 
 
 function init() {
@@ -22,8 +55,8 @@ function init() {
     scene.background = new THREE.Color(0x36393f);
 
 
-    camera.position.set(-1, -0.5, 2);
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    camera.position.set(0, -0.2, 0.5);
+    camera.lookAt(new THREE.Vector3(0, -0.2, 0));
 
     // hlight = new THREE.AmbientLight (0x021670,0.75);
     // scene.add(hlight);
@@ -47,8 +80,7 @@ function init() {
     light4 = new THREE.PointLight(0xc4c4c4,5);
     light4.position.set(-500,300,500);
     scene.add(light4);*/
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
+UpdateSize();
 
     let loader = new GLTFLoader();
     loader.load('./models/kitchenFridgeLarge' +
@@ -59,28 +91,57 @@ function init() {
         scene.add(gltf.scene);
         render();
     });
-    AddImage('https://cdn.discordapp.com/attachments/814021494069395466/814032080811458590/86757885_10157913009857510_1262544196875583488_o.jpg');
+
+    AddPictures();
+
     // animate();
 }
+function UpdateSize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-async function AddImage(url) {
-    var geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
-    var material = await GetMaterial(url);
+}
+
+async function AddPictures() {
+
+    var allPictures = await GetFridgeImageFileNames(new URLSearchParams(window.location.search).get('key'));
+
+    for (var i = allPictures.length-1; i >= 0; i--) {
+
+        await AddImage(allPictures[i], i);
+    }
+}
+
+async function AddImage(imageURL, index) {
+    if (index >= imagePositions.length) {
+        return;
+    }
+
+    var geometry = new THREE.PlaneGeometry(1, 1, 1, 1,);
+    var material = await GetMaterial('https://fridgeserver.herokuapp.com/' + imageURL);
+
+    var size = [material.map.image.width, material.map.image.height];
+    var aspectRatio = size[0]/size[1];//: size[1]/size[0];
 
     var plane = new THREE.Mesh(geometry, material);
+    var currentPosition = imagePositions[imagePositions.length - (index + 1)];
+    var scale = currentPosition[2] || 1;
+    scale *= 0.075;
+    plane.scale.set(aspectRatio * scale,  scale, 1);
+    
+    plane.position.set(currentPosition[0], currentPosition[1], 0.041);
     scene.add(plane);
+    render();
 }
 
 async function GetMaterial(url) {
 
     return new Promise(success => {
-        LoadDiscordImage(url).then(image => {
-            var loader = new THREE.TextureLoader();
-            loader.load(image, texture => {
-                var mat = new THREE.MeshBasicMaterial({map: texture, side: THREE.DoubleSide});
-                success(mat);
+        var loader = new THREE.TextureLoader();
+        loader.load(url, texture => {
+            var mat = new THREE.MeshBasicMaterial({map: texture, side: THREE.DoubleSide});
+            success(mat);
 
-            });
         });
 
     });
@@ -89,24 +150,24 @@ async function GetMaterial(url) {
 function LoadDiscordImage(url) {
     return new Promise(success => {
 
-       /* var thisOrigin = '127.0.0.1:8887';
-        var headers = new Headers();
-        //headers.append('Access-Control-Allow-Origin', '*');
-        
-        headers.append('Access-Control-Request-Headers','access-control-request-headers: access-control-allow-headers,access-control-allow-origin');
-        headers.append("Access-Control-Request-Method", "GET");
-        headers.append('Origin',thisOrigin);
-        //headers.append('Access-Control-Allow-Origin', '*');
+        /* var thisOrigin = '127.0.0.1:8887';
+         var headers = new Headers();
+         //headers.append('Access-Control-Allow-Origin', '*');
+         
+         headers.append('Access-Control-Request-Headers','access-control-request-headers: access-control-allow-headers,access-control-allow-origin');
+         headers.append("Access-Control-Request-Method", "GET");
+         headers.append('Origin',thisOrigin);
+         //headers.append('Access-Control-Allow-Origin', '*');
+ 
+         var request = new Request(url, {
+             headers:headers,
+             method:'OPTIONS'
+             /* referrerPolicy: 'no-referrer',
+              credentials: 'same-origin',
+              cache: 'no-cache'
+         });*/
 
-        var request = new Request(url, {
-            headers:headers,
-            method:'OPTIONS'
-            /* referrerPolicy: 'no-referrer',
-             credentials: 'same-origin',
-             cache: 'no-cache'
-        });*/
-        
-        fetch('https://fridgeserver.herokuapp.com/').then(result => {
+        fetch('').then(result => {
             console.log(result);
 
             success(result);
